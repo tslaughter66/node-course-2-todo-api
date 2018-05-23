a85587c0b0db6eb576445f5884d012c9ac6688c9
Tim Slaughter <tslaughter66@yahoo.com>
2018-05-22 18:37:28 -0700

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../model/todo');
 
 // Make up an array of dummy todos to fill database for /GET.
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -79,5 +82,32 @@ describe('GET /todos', () => {
         expect(res.body.todos.length).toBe(2);
       })
       .end(done);
-  })
+  });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return a todo doc',(done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)   // get the id from the created todos (see beforeEach above).
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString(); // create a new OBjectID which means it won't be in the DB
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get(`/todos/12345`)    // pass in random id number that doesn't have ObjectID structure
+      .expect(404)
+      .end(done);
+  });
 });
